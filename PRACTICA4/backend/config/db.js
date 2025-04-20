84376ffa1b60f097e8c4bcda238cf1ccf115aba6
ALEXAGAM99 <aargollom@fcpn.edu.bo>
2025-04-20 16:35:53 -0400

    Delete PRACTICA4 directory

--- PRACTICA4/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "hospital"
-}).promise();
-
-export default pool;
